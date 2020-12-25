186e0df2073f8be54b57194423f67b29649a3139
Tarik Dzambic <33522753+DzambaTz@users.noreply.github.com>
2020-12-25 22:45:05 +0100

    Revert "Promijenjen uslov u while petlji"

--- zadaca2/z4/main.cpp
+++ zadaca2/z4/main.cpp
@@ -29,8 +29,6 @@ double unos_elemenata_i_racun_prosjeka(kontejner &lista) {
   std::cout << "Unesite elemente u listu: " << std::endl;
   double x;
   double suma = 0.;
-  cin.clear();
-  // cin.ignore();
   while (cin >> std::noskipws >> x) {
     lista.push_back(x);
     suma += x;
@@ -49,7 +47,7 @@ void ispis_elemenata(const kontejner &lista) {
 
 int main() {
   kontejner lista1;
-  while ( lista1.empty() ) {
+  while (true) {
     try {
       auto p = unos_elemenata_i_racun_prosjeka(lista1);
       auto m = median(lista1);
@@ -58,6 +56,8 @@ int main() {
       break;
     } catch (const std::string &e) {
       std::cout << "Greska: " << e << std::endl;
+      cin.clear();
+      cin.ignore();
       continue;
     }
   }
